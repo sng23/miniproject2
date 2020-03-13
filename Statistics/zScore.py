2c5325f015494632768dd787400a42a4dd5647b0
jeffrey <jpf37@njit.edu>
2020-03-12 21:35:15 -0400

    feature: added zscore funtion

---
+++ Statistics/zScore.py
@@ -0,0 +1,20 @@
+
+from scipy import stats
+from Statistics.Mean import mean
+from Calculator.Subtraction import fn_subtraction
+from numpy import std
+from Statistics.standard_Deviation import standard
+from Calculator.Division import fn_division
+
+
+#z = (x - mean) / stdev; x is for raw score
+def z(data):
+    x = 25
+    b = standard(data)
+    c = mean(data)
+    d = fn_subtraction(x, c)
+    return round(fn_division(d, b), 5)
+
+#def z(data):
+ #   return stats.zscore(data)
+
